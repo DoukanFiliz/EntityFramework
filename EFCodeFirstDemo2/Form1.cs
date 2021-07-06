7f9ff02924ca84ee266e438cd60ef2bbb709ecae
Dogukan Filiz <dogukanfiliz@hotmail.com>
2021-07-06 23:30:24 +0300

    Add project files.

---
+++ EFCodeFirstDemo2/Form1.cs
@@ -0,0 +1,87 @@
+﻿using EFCodeFirstDemo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EFCodeFirstDemo2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ProductDal productDal = new ProductDal();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            dataGridView1.DataSource = productDal.GetAll();
+        }
+
+        private void SearchData(string key)
+        {
+            //dataGridView1.DataSource = productDal.GetAll().Where(p => p.Name.ToString().ToLower().Contains(key.ToLower())).ToList();
+            dataGridView1.DataSource = productDal.GetByName(key);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            productDal.Add(new Product
+            {
+                Name = textBox1.Text,
+                UnitPrice = Convert.ToDecimal(textBox2.Text),
+                StockAmount = Convert.ToInt32(textBox3.Text)
+            });
+            LoadData();
+            MessageBox.Show("Product Added!", "Succesful");
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox6.Text.Length < 1)
+            {
+                MessageBox.Show("Some informations are missing!", "Warning");
+            }
+            else
+            {
+                productDal.Update(new Product
+                {
+                    Id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value),
+                    Name = textBox6.Text.ToString(),
+                    UnitPrice = Convert.ToDecimal(textBox5.Text),
+                    StockAmount = Convert.ToInt32(textBox4.Text)
+                });
+                LoadData();
+                MessageBox.Show("Product Updated!", "Succesful");
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox6.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            textBox5.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            textBox4.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            productDal.Remove(new Product { Id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value) });
+            LoadData();
+            MessageBox.Show("Product Removed!", "Succesful");
+        }
+
+        private void tbxSearch_TextChanged(object sender, EventArgs e)
+        {
+            SearchData(tbxSearch.Text);
+        }
+    }
+}
---
+++ EFCodeFirstDemo2/ProductDal.cs
@@ -0,0 +1,55 @@
+﻿using EFCodeFirstDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCodeFirstDemo2
+{
+    class ProductDal
+    {
+        public List<Product> GetAll()
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+        public List<Product> GetByName(string key)
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                return context.Products.Where(p => p.Name.ToString().Contains(key)).ToList();
+            }
+        }
+        public void Add(Product product)
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                //context.Products.Add(product);
+                var entity = context.Entry(product);
+                entity.State = System.Data.Entity.EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+        public void Update(Product product)
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                var entity = context.Entry(product);
+                entity.State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        public void Remove(Product product)
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                var entity = context.Entry(product);
+                entity.State = System.Data.Entity.EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+    }
+}
